293d47ecc4efe150f775aa6a37389200095804fa
Judaspk <95680691+Judaspk@users.noreply.github.com>
2024-07-07 19:26:13 -0600

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -29,7 +29,7 @@ module.exports = {
 
   run: async (client, interaction) => {
     try {
-      const youtubeLink = 'https://discord.gg/xQF9f9yUEM';
+      const youtubeLink = 'https://discord.gg/3SyBw2CzBg';
       const InstagramLink = 'https://www.instagram.com/rtxxgg/';
       const { EmbedBuilder } = require('discord.js')
         const embed = new EmbedBuilder()
@@ -37,7 +37,7 @@ module.exports = {
             .setAuthor({
           name: 'Owner',
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157310253520662638/2443-iconperson.png?ex=651824aa&is=6516d32a&hm=0becc4a0fda01e5a02a63cf098db30c287e60a474f8d2da4ddeae7f47d98a5a3&',
-          url: 'https://discord.gg/FUEHs7RCqz'
+          url: 'https://discord.gg/3SyBw2CzBg'
         })
             .setDescription(`__**About me**__:\n\n ▶️ Myself Shiva aka GlaceYT. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n YouTube : ❤️ [RTX GAMING](${youtubeLink})\n Instagram : 💙 [rtxxgg](${InstagramLink})`)
             .setTimestamp();
